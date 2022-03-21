bea6d53abff2b228a70a22383c2123456cf83f92
abooabdirahman <abuabdirahman6@gmail.com>
2022-03-21 15:40:21 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * puts2 - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+	if (i % 2 == 0)
+	{
+		putchar(str[i]);
+	}
+
+	i++;
+}
+putchar('\n');
+}
